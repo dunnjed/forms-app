1e14ebb8fb965e0101f6d34077ea39ece9057ecd
JD <none>
2017-05-18 01:36:44 -0400

    Added logic for buttons and styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  options = [
+    { step: 'Attend Class' },
+    { step: 'Practice' },
+    {
+      step: [
+        // { step: '' },
+        { step: 'Homework' },
+        { step: 'Study' }
+      ]
+    },
+    { step: 'Review' },
+    { step: 'Test' }
+  ];
+
+  dropDownOptions = [];
+  stepIndex = 0;
+  currentStep = this.options[this.stepIndex];
+  showMultipleOptions = false;
+  disableBtn = false;
+
+  constuctor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  completeThisStepFromBtn() {
+    this.stepIndex++;
+    if (this.stepIndex < this.options.length) {
+      this.currentStep = this.options[this.stepIndex];
+      if (Array.isArray(this.currentStep.step)) {
+        this.dropDownOptions = this.currentStep.step;
+        this.showMultipleOptions = true;
+      } else {
+        this.showMultipleOptions = false;
+
+        if (this.stepIndex === this.options.length - 1) {
+          this.disableBtn = true;
+        }
+      }
+    }
+  }
+
+  completeThisStepFromSelection(step) {
+    console.log(step);
+    _.remove(this.dropDownOptions, element => {
+      return element.step === step;
+    });
+    if (this.dropDownOptions.length === 0) {
+      this.showMultipleOptions = false;
+      this.stepIndex++;
+      this.currentStep = this.options[this.stepIndex];
+    }
+  }
 }
